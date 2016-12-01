023bdd96fd35c2a44b298531375d22dab0db2864
wyzc <wahewe@163.com>
2016-12-01 13:51:10 +0800

    第一次添加空项目

---
+++ text/text/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text
+//
+//  Created by wyzc on 2016/12/1.
+//  Copyright © 2016年 wyzc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ text/text/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  text
+//
+//  Created by wyzc on 2016/12/1.
+//  Copyright © 2016年 wyzc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
